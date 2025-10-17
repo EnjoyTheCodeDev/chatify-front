2b975d8ddb1d380b514cd936df8d2b354cb33e0a
EnjoyTheCodeDev <enjoy.the.code.dev@gmail.com>
2025-10-17 14:42:10 +0200

    changed api url env name

--- src/utils/api/fetch.ts
+++ src/utils/api/fetch.ts
@@ -1,6 +1,6 @@
 import { AuthService } from "../services/authService";
 
-const BASE_URL = import.meta.env.API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function apiFetch<T>(
   url: string,
